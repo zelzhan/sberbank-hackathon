0e0143238ac00ae18827f630b56f34210ab35d34
daukadolt <daulet.amirkhanov.official@gmail.com>
2018-05-05 21:33:38 +0600

    database switched from /elzhan to /sberbank

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
     // url: "mongodb://dauleta:dauletdoka1@ds115740.mlab.com:15740/heroku_ww40lf1b"
-    url: "mongodb://localhost:9000/elzhan"
+    url: "mongodb://localhost:9000/sberbank"
 }
